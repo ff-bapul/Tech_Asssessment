53973ecd3b9d59d574bed5aa9b253b575864c729
blabade <blabade@fpb6a80183.ap.nuro.jp>
2024-10-26 11:58:08 +0530

    Added empty value use case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,7 +2,14 @@
 require 'spec_helper'
 require_relative '../string_calculator'  # Adjust the path as needed
 
-RSpec.describe 'string calculator' do
-  it '' do
+# string_calculator_spec.rb
+require './string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(StringCalculator.add("")).to eq(0)
+    end
   end
 end
+
