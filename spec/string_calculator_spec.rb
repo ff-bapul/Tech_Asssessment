3b8b755d189ded69f1fd486dc30cd2ca767e678f
blabade <blabade@fpb6a80183.ap.nuro.jp>
2024-10-26 11:48:53 +0530

    Initial project setup

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,8 @@
+# spec/your_code_spec.rb
+require 'spec_helper'
+require_relative '../string_calculator'  # Adjust the path as needed
+
+RSpec.describe 'string calculator' do
+  it '' do
+  end
+end
